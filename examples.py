94667f1fd45ac7ba16e49f17071787f438708439
lukagrbcic <luka.grbcic@gmail.com>
2024-10-01 16:07:06 -0700

    added examples, moved benchmarks.py

---
+++ examples.py
@@ -0,0 +1,59 @@
+import sys 
+import numpy as np
+
+
+sys.path.insert(0, 'src')
+
+from benchmarks import *
+
+
+name = 'inconel_benchmark'
+model = load_model(name).load_model()
+f = benchmark_functions(name, model)
+lb, ub = f.get_bounds()
+def evaluation_function(x):
+    
+    value = f.evaluate(x)
+    
+    return value
+
+emissivity = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
+
+
+name = 'airfoil_benchmark'
+model = load_model(name).load_model()
+f = benchmark_functions(name, model)
+lb, ub = f.get_bounds()
+
+def evaluation_function(x):
+    
+    value = f.evaluate(x)
+
+    return value
+
+cp = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
+
+
+name = 'scalar_diffusion_benchmark'
+model = load_model(name).load_model()
+f = benchmark_functions(name, model)
+lb, ub = f.get_bounds()
+
+def evaluation_function(x):
+    value = f.evaluate(x)
+    return value
+
+probes = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
+
+
+name = 'friedman_multioutput_benchmark'
+f = benchmark_functions(name)
+lb, ub = f.get_bounds()
+
+def evaluation_function(x):
+    value = f.evaluate(x)
+    return value
+
+y = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
+
+
