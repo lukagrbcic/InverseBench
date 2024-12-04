34f71bc3f8f113d970258b9930fb29c2129fb2fc
lukagrbcic <luka.grbcic@gmail.com>
2024-12-04 11:32:49 -0800

    examples updated

--- examples.py
+++ examples.py
@@ -1,11 +1,5 @@
-import sys 
 import numpy as np
-
-
-sys.path.insert(0, 'src')
-
-from benchmarks import *
-
+from InverseBench.benchmarks import load_model, benchmark_functions
 
 name = 'inconel_benchmark'
 model = load_model(name).load()
@@ -32,28 +26,5 @@ def evaluation_function(x):
     return value
 
 cp = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
-
-
-name = 'scalar_diffusion_benchmark'
-model = load_model(name).load()
-f = benchmark_functions(name, model)
-lb, ub = f.get_bounds()
-
-def evaluation_function(x):
-    value = f.evaluate(x)
-    return value
-
-probes = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
-
-
-name = 'friedman_multioutput_benchmark'
-f = benchmark_functions(name)
-lb, ub = f.get_bounds()
-
-def evaluation_function(x):
-    value = f.evaluate(x)
-    return value
-
-y = evaluation_function([np.random.uniform(lb, ub), np.random.uniform(lb, ub)])
 
 
