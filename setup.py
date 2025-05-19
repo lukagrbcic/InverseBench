7efa204cf43ec9f6cc93dcf1a2bca2ec04fd430a
lukagrbcic <luka.grbcic@gmail.com>
2025-05-19 08:41:58 -0700

    setup version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InverseBlackBox',
-    version='0.1.0',      
+    version='0.0.1',
     author='Luka Grbcic',      
     author_email='lgrbcic@lbl.gov',      
     description='An inverse design black box problem benchmark suite',
